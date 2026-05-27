6c6fa7f5e84b845e6ac1a3984aebdb20cfec0f45
Harry S <harry@LDN-LAP-997576.corp.com>
2026-05-27 09:29:15 +0100

    feat: implement tf-atom-wafv2-web-acl-aws resource

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,18 @@ output "enabled" {
   description = "Whether the module is enabled."
   value       = local.enabled
 }
+
+output "id" {
+  description = "The ID of the Web ACL."
+  value       = try(aws_wafv2_web_acl.default[0].id, "")
+}
+
+output "arn" {
+  description = "The ARN of the Web ACL."
+  value       = try(aws_wafv2_web_acl.default[0].arn, "")
+}
+
+output "capacity" {
+  description = "The capacity units used by the Web ACL."
+  value       = try(aws_wafv2_web_acl.default[0].capacity, 0)
+}
