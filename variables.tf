826bf1875c6dc98304542c89135719dc61424324
Harry S <harry@LDN-LAP-997576.corp.com>
2026-05-27 09:28:33 +0100

    feat: implement tf-atom-wafv2-web-acl-aws resource

--- variables.tf
+++ variables.tf
@@ -1,7 +1,46 @@
 # -----------------------------------------------------------------------------
 # Module-Specific Variables
-#
-# Note: Standard labeling variables (enabled, namespace, tenant, environment,
-# stage, name, delimiter, attributes, tags, label_order, etc.) are provided
-# by context.tf via the tf-label module.
 # -----------------------------------------------------------------------------
+
+variable "scope" {
+  type        = string
+  description = "Scope of the WAF Web ACL. Valid values: REGIONAL, CLOUDFRONT."
+  validation {
+    condition     = contains(["REGIONAL", "CLOUDFRONT"], var.scope)
+    error_message = "Scope must be REGIONAL or CLOUDFRONT."
+  }
+}
+
+variable "description" {
+  type        = string
+  description = "Description of the Web ACL."
+  default     = ""
+}
+
+variable "default_action" {
+  type        = string
+  description = "Default action for the Web ACL. Valid values: allow, block."
+  default     = "allow"
+  validation {
+    condition     = contains(["allow", "block"], var.default_action)
+    error_message = "Default action must be allow or block."
+  }
+}
+
+variable "rules" {
+  type        = any
+  description = "List of rule objects for the Web ACL."
+  default     = []
+}
+
+variable "cloudwatch_metrics_enabled" {
+  type        = bool
+  description = "Whether CloudWatch metrics are enabled for the Web ACL."
+  default     = true
+}
+
+variable "sampled_requests_enabled" {
+  type        = bool
+  description = "Whether sampled requests are enabled."
+  default     = true
+}
